63cada04f13bc095e4a95f6664392c37600ccc1c
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2019-08-03 15:53:16 +0900

    [update] 直近3ヶ月以内に登録されたもののみ流れるように

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @video_ids = Video.pluck(:id).shuffle
+    @video_ids = Video.where('created_at >= ?', 3.month.ago).pluck(:id).shuffle
   end
 end
